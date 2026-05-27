5e8c3d267a591c125888161cf31754dfc90ca8da
L1m3x <48759715+L1m3x@users.noreply.github.com>
2026-05-27 18:05:10 +0200

    sodoma i gomora

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -5,8 +5,40 @@ namespace Managers
 {
     public class AudioManager : SingletonPersistant<AudioManager>
     {
-        public void PlaySFX(string SFXName) {}
-        public void PlayMusic(string MusicName) {}
+        [SerializeField] private AudioClip backgroundMusic;
+        [SerializeField, Range(0f, 1f)] private float musicVolume = 1f;
+
+        private AudioSource _musicSource;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (Instance != this)
+            {
+                return;
+            }
+
+            _musicSource = GetComponent<AudioSource>();
+            if (_musicSource == null)
+            {
+                _musicSource = gameObject.AddComponent<AudioSource>();
+            }
+
+            _musicSource.playOnAwake = false;
+            _musicSource.loop = true;
+            _musicSource.volume = musicVolume;
+            _musicSource.clip = backgroundMusic;
+
+            if (backgroundMusic != null && !_musicSource.isPlaying)
+            {
+                _musicSource.Play();
+            }
+        }
+
+        public void PlaySfx(string sfxName) {}
+
+        public void PlayMusic(string musicName) {}
     }    
 }
 
